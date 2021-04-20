dd4c7e0a3a61a384fe3437ce7f8f8b59ba01113e
Qosimjon <33579671+Qosimjon1997@users.noreply.github.com>
2021-04-20 12:37:32 +0500

    Qosimjon3

--- AntConc/MainWindow.xaml.cs
+++ AntConc/MainWindow.xaml.cs
@@ -306,7 +306,6 @@ namespace AntConc
                 char[] separator = { ',', '\n', '\t', ' ', '\b', '.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', '\"', '[', ']', '(', ')' };
                 var myFile = (File.ReadAllText(myOpenedFile)).Split(separator);
                 var hashSet = new HashSet<string>(myFile);
-                labWordType4.Text = (hashSet.Count - 1).ToString();
 
                 var v = myFile.GroupBy(s => s).OrderByDescending(s => s.Count()).ThenBy(n => n.Key).ToList();
 
@@ -353,7 +352,6 @@ namespace AntConc
                         }
 
                         worksheet.Columns("A", "Z").AdjustToContents();
-                        //  string sql = "SELECT Dorilars.Id as id,Partiyas.Id as t_id,  TovarNomi as Nomi, IshlabChiqaruvchi as Ishlab_chiqaruvchi,Nomi as Turi,Doza,(BazaviyNarx) as Olish_Narxi,SotiladiganNarx as Sotiladigan_Narx,NechtaKeldiDona/PachkadaNechta as Necha_Pachka,NechtaKeldiDona%PachkadaNechta as Necha_Dona,PachkadaNechta as Pachkada_soni, Shtrix as Shtrix_Kod, CONVERT(varchar, KelganSana, 3) as kelgan_sana From Dorilars,Partiyas,DorilarTuris where Dorilars.DorilarTuriId=DorilarTuris.Id and Dorilars.id=Partiyas.DorilarId;";
 
                         workbook.SaveAs(filename);
                         Process cmd = new Process();
@@ -378,5 +376,248 @@ namespace AntConc
                 MessageBox.Show("File not selected!");
             }
         }
+
+        private void btnTab4Convert_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var myFile = (File.ReadAllText(myOpenedFile)).Split();
+                int ttt = myFile.Count();
+                int i = 0;
+                List<int> indexOfFind = new List<int>();
+                foreach (var q in myFile)
+                {
+                    if (q.Contains(txtTab4Search.Text))
+                    {
+                        indexOfFind.Add(i);
+                    }
+
+                    i++;
+                }
+                List<ConcordanceHits> myTable = new List<ConcordanceHits>();
+                int p = 1;
+                foreach (var t in indexOfFind)
+                {
+                    string S = "";
+                    for (int k = 0; k < Convert.ToInt32(myUpDown1.Value) - 1; k++)
+                    {
+                        try
+                        {
+                            S += myFile[t + k] + " ";
+                        }
+                        catch (Exception ex)
+                        {
+
+                        }
+                    }
+                    for (int k = Convert.ToInt32(myUpDown1.Value) - 1; k < Convert.ToInt32(myUpDown2.Value); k++)
+                    {
+                        try
+                        {
+                            S += myFile[t + k] + " ";
+                            myTable.Add(new ConcordanceHits() { Id = p, myWords = S });
+                            p++;
+                        }
+                        catch (Exception ex)
+                        {
+
+                        }
+                    }
+
+
+
+                }
+
+                Stream myStream;
+                SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+                saveFileDialog1.Filter = "EXCELL FILE (*.xlsx)|*.xlsx";
+                saveFileDialog1.RestoreDirectory = true;
+                string filename;
+                if (saveFileDialog1.ShowDialog() == true)
+                {
+                    if ((myStream = saveFileDialog1.OpenFile()) != null)
+                    {
+                        // Code to write the stream goes here.
+                        filename = saveFileDialog1.FileName;
+                        myStream.Close();
+
+                        var workbook = new XLWorkbook();
+                        IXLWorksheet worksheet = workbook.Worksheets.Add("Sheet1");
+                        worksheet.Columns().AdjustToContents();
+                        worksheet.Cell(1, 1).Value = "Fayl : " + _XXX;
+                        worksheet.Cell(1, 1).Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
+
+                        worksheet.Cell(2, 1).Value = "Qidirilayotgan so'z : " + txtTab4Search.Text;
+                        worksheet.Cell(2, 1).Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
+
+                        worksheet.Cell(3, 1).Value = "Klaster turlarining umumiy soni : " + labNumberHits4.Text;
+                        worksheet.Cell(3, 1).Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
+
+                        worksheet.Cell(4, 1).Value = "Min. so'z: " + myUpDown1.Value + " Max. so'z: " + myUpDown2.Value;
+                        worksheet.Cell(4, 1).Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
+
+                        worksheet.Range(worksheet.Cell(1, 1), worksheet.Cell(1, 2)).Merge();
+                        worksheet.Range(worksheet.Cell(2, 1), worksheet.Cell(2, 2)).Merge();
+                        worksheet.Range(worksheet.Cell(3, 1), worksheet.Cell(3, 2)).Merge();
+                        worksheet.Range(worksheet.Cell(4, 1), worksheet.Cell(4, 2)).Merge();
+                        worksheet.Cell(5, 1).Value = "Darajasi";
+                        worksheet.Cell(5, 2).Value = "Xillar";
+
+                        int kk = 6;
+                        foreach (ConcordanceHits rv in myTable)
+                        {
+                            worksheet.Cell(kk, 1).Value = rv.Id;
+                            worksheet.Cell(kk, 2).Value = rv.myWords;
+                            kk++;
+                        }
+
+                        worksheet.Columns("A", "Z").AdjustToContents();
+
+                        workbook.SaveAs(filename);
+                        Process cmd = new Process();
+                        cmd.StartInfo.FileName = "cmd.exe";
+                        cmd.StartInfo.RedirectStandardInput = true;
+                        cmd.StartInfo.RedirectStandardOutput = true;
+                        cmd.StartInfo.CreateNoWindow = true;
+                        cmd.StartInfo.UseShellExecute = false;
+                        cmd.Start();
+
+                        cmd.StandardInput.WriteLine(filename);
+                        cmd.StandardInput.Flush();
+                        cmd.StandardInput.Close();
+                        cmd.WaitForExit();
+                        //  workbook.
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("File not selected!");
+            }
+
+        }
+
+        private void btnTab1Convert_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var myFile = (File.ReadAllText(myOpenedFile)).Split();
+
+                int i = 0;
+                List<int> indexOfFind = new List<int>();
+                foreach (var q in myFile)
+                {
+                    if (q.Contains(txtTab1Search.Text))
+                    {
+                        indexOfFind.Add(i);
+                    }
+
+                    i++;
+                }
+                List<WordTypes> conword = new List<WordTypes>();
+                foreach (var t in indexOfFind)
+                {
+                    string S = "";
+                    if (t >= 7)
+                    {
+                        for (int u = 7; u >= 0; u--)
+                        {
+                            S += " " + myFile[t - u];
+                        }
+                    }
+                    else
+                    {
+                        for (int u = t % 7; u >= 0; u--)
+                        {
+                            S += " " + myFile[t - u];
+                        }
+                    }
+
+                    if (t + 7 <= myFile.Count())
+                    {
+                        for (int u = 1; u <= 7; u++)
+                        {
+                            S += " " + myFile[t + u];
+                        }
+                    }
+                    else
+                    {
+                        for (int u = 1; u <= u - myFile.Count(); u++)
+                        {
+                            S += " " + myFile[t + u];
+                        }
+                    }
+                    conword.Add(new WordTypes { Word = S });
+                }
+
+                Stream myStream;
+                SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+                saveFileDialog1.Filter = "EXCELL FILE (*.xlsx)|*.xlsx";
+                saveFileDialog1.RestoreDirectory = true;
+                string filename;
+                if (saveFileDialog1.ShowDialog() == true)
+                {
+                    if ((myStream = saveFileDialog1.OpenFile()) != null)
+                    {
+                        // Code to write the stream goes here.
+                        filename = saveFileDialog1.FileName;
+                        myStream.Close();
+
+                        var workbook = new XLWorkbook();
+                        IXLWorksheet worksheet = workbook.Worksheets.Add("Sheet1");
+                        worksheet.Columns().AdjustToContents();
+                        worksheet.Cell(1, 1).Value = "Fayl : " + _XXX;
+                        worksheet.Cell(1, 1).Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
+
+                        worksheet.Cell(2, 1).Value = "Qidirilayotgan so'z : " + txtTab1Search.Text;
+                        worksheet.Cell(2, 1).Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
+
+                        worksheet.Cell(3, 1).Value = "Klaster turlarining umumiy soni : " + labNumberHits.Text;
+                        worksheet.Cell(3, 1).Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
+
+
+                        worksheet.Range(worksheet.Cell(1, 1), worksheet.Cell(1, 2)).Merge();
+                        worksheet.Range(worksheet.Cell(2, 1), worksheet.Cell(2, 2)).Merge();
+                        worksheet.Range(worksheet.Cell(3, 1), worksheet.Cell(3, 2)).Merge();
+
+                        int kk = 4;
+                        foreach (WordTypes rv in conword)
+                        {
+                            worksheet.Cell(kk, 1).Value = rv.Word;
+                            kk++;
+                        }
+
+                        worksheet.Columns("A", "Z").AdjustToContents();
+                        //  string sql = "SELECT Dorilars.Id as id,Partiyas.Id as t_id,  TovarNomi as Nomi, IshlabChiqaruvchi as Ishlab_chiqaruvchi,Nomi as Turi,Doza,(BazaviyNarx) as Olish_Narxi,SotiladiganNarx as Sotiladigan_Narx,NechtaKeldiDona/PachkadaNechta as Necha_Pachka,NechtaKeldiDona%PachkadaNechta as Necha_Dona,PachkadaNechta as Pachkada_soni, Shtrix as Shtrix_Kod, CONVERT(varchar, KelganSana, 3) as kelgan_sana From Dorilars,Partiyas,DorilarTuris where Dorilars.DorilarTuriId=DorilarTuris.Id and Dorilars.id=Partiyas.DorilarId;";
+
+                        workbook.SaveAs(filename);
+                        Process cmd = new Process();
+                        cmd.StartInfo.FileName = "cmd.exe";
+                        cmd.StartInfo.RedirectStandardInput = true;
+                        cmd.StartInfo.RedirectStandardOutput = true;
+                        cmd.StartInfo.CreateNoWindow = true;
+                        cmd.StartInfo.UseShellExecute = false;
+                        cmd.Start();
+
+                        cmd.StandardInput.WriteLine(filename);
+                        cmd.StandardInput.Flush();
+                        cmd.StandardInput.Close();
+                        cmd.WaitForExit();
+                        //  workbook.
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("File not selected!");
+            }
+
+        }
+
+
     }
 }
